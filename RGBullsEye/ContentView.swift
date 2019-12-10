4ad96644c2fedb7f465a4c06a8ebffcd8df043e7
sxc <qiuyequ@icloud.com>
2019-12-10 15:07:25 +0800

    modify the initializer in SceneDelegate

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,6 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
     var body: some View {
         VStack {
             HStack {
@@ -32,6 +39,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().previewLayout(.fixed(width: 568, height: 320))
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 568, height: 320))
     }
 }
